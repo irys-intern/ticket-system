45fe8b6ad16b292a7363ee1aed40de8fd3b0ae9c
Eli Friedman <elifriedman@irystechnologies.com>
2026-06-08 10:56:35 -0500

    fix env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -24,13 +24,5 @@ export const env = {
     url: process.env.FRONTEND_URL || 'http://localhost:5173',
   },
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   superPassword: process.env.SUPERUSER_PASSWORD,
-=======
-  superPassword: process.env.SUPERUSER_PASSWORD || 'super-secret-password',
->>>>>>> dashboards
-=======
-  superPassword: process.env.SUPERUSER_PASSWORD || 'super-secret-password',
->>>>>>> dashboards
 };
