273af153a7e4174f6907229ee22d673d08c3b624
Eli Friedman <elifriedman@irystechnologies.com>
2026-08-03 12:40:14 -0500

    remove default redis path

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -13,7 +13,7 @@ export const env = {
   },
   
   redis: {
-    url: process.env.REDIS_URL || 'redis://localhost:6379',
+    url: process.env.REDIS_URL// || 'redis://localhost:6379',
   },
 
   nlp: {
